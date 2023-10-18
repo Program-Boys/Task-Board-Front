ca53976f48ba487411ec385fd42dec85b1c4850f
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-10-18 17:36:37 -0300

    Task container component created

--- front-tasks/src/components/TasksContainer/tasksContainer.tsx
+++ front-tasks/src/components/TasksContainer/tasksContainer.tsx
@@ -1,5 +1,28 @@
+import { Grid, Typography } from "@mui/material";
+import TaskList from "../TaskList/taskList";
+
 const TasksContainer = () => {
-  return <>{/*Aqui vai o container das tasks*/}</>;
+  return (
+    <>
+      <Grid
+        container
+        sx={{
+          p: "15px",
+        }}
+      >
+        <Typography variant="h5" color={"var(--gray)"} mb={"10px"}>
+          Main Board
+        </Typography>
+
+        <Grid container>
+          <TaskList
+            name="Minhas tarefas"
+            tasks={[{ name: "Treinamento", id: "1" }]}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default TasksContainer;
