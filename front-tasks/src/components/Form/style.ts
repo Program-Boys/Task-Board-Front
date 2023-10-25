c035e03b3130c7beff0cb77af2fb3d712b35616d
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-10-25 17:16:19 -0300

    Form component created

---
+++ front-tasks/src/components/Form/style.ts
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 15px;
+  width: 300px;
+
+  button {
+    text-transform: none;
+  }
+`;
