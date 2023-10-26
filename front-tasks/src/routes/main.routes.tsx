b3dd92281bdfdd155b2a93516a4ce1fa9fefa377
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-10-26 17:09:43 -0300

    Private route added in main routes

--- front-tasks/src/routes/main.routes.tsx
+++ front-tasks/src/routes/main.routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "../pages/Login/login.pages";
 import Register from "../pages/Register/register.pages";
 import Task from "../pages/Tasks/tasks.pages";
+import PrivateRoute from "./privateRoute.routes";
 
 const MainRoutes = () => {
   const location = useLocation();
@@ -10,7 +11,14 @@ const MainRoutes = () => {
     <Routes location={location} key={location.pathname}>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/tasks" element={<Task />} />
+      <Route
+        path="/tasks"
+        element={
+          <PrivateRoute redirectTo="/">
+            <Task />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 };
