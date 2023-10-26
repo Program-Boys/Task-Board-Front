71c393a896c5c0c8c8f3dc31305f450c5d17fa6a
Enrique Barbosa <e.barbosa@grouplinknetwork.com>
2023-10-26 17:07:06 -0300

    Services file

---
+++ front-tasks/src/services/services.ts
@@ -0,0 +1,18 @@
+import { IFormData } from "../pages/Login/login.pages";
+import api from "./api";
+
+const Services = () => {
+  async function login(data: IFormData) {
+    const response = api.post("/login", data);
+
+    return response;
+  }
+
+  //aqui vão os outros serviços
+
+  return {
+    login,
+  };
+};
+
+export { Services };
