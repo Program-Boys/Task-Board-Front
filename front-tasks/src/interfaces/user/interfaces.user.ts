da1cadd2aa86c507f2ef96efde02a308c2563cb3
Guilherme <guilhermeshadow91@gmail.com>
2023-11-26 16:37:34 -0300

    feat: interfaces folder

---
+++ front-tasks/src/interfaces/user/interfaces.user.ts
@@ -0,0 +1,11 @@
+export interface IFormLoginData {
+  email?: string;
+  password?: string;
+}
+
+export interface IFormRegisterData {
+  name?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+}
